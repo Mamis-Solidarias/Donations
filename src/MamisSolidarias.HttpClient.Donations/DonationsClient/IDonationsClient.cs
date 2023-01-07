83bc32af33c0db21286779b6dfefd7e2291ab673
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-01-07 11:27:22 -0300

    [Chore] Removed template stuff

--- src/MamisSolidarias.HttpClient.Donations/DonationsClient/IDonationsClient.cs
+++ src/MamisSolidarias.HttpClient.Donations/DonationsClient/IDonationsClient.cs
@@ -1,8 +1,5 @@
-using MamisSolidarias.WebAPI.Donations.Endpoints.Test;
-
 namespace MamisSolidarias.HttpClient.Donations.DonationsClient;
 
 public interface IDonationsClient
 {
-    Task<Response?> GetTestAsync(Request requestParameters, CancellationToken token = default);
 }
