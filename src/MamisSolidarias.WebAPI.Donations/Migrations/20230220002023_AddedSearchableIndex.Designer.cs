defefa8dddf87dac490f0ebd3981ffe9f81ecc19
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-02-19 21:29:51 -0300

    [Feature] Updated db model to add searchable field

--- src/MamisSolidarias.WebAPI.Donations/Migrations/20230220002023_AddedSearchableIndex.Designer.cs
+++ src/MamisSolidarias.WebAPI.Donations/Migrations/20230220002023_AddedSearchableIndex.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MamisSolidarias.Infrastructure.Donations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MamisSolidarias.WebAPI.Donations.Migrations
 {
     [DbContext(typeof(DonationsDbContext))]
-    partial class DonationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230220002023_AddedSearchableIndex")]
+    partial class AddedSearchableIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
